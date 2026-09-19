89fd5b3b0cfc3b76533d9630573dd255921c9ec3
Mohamed OuledAli <mbmohamed195@gmail.com>
2026-09-20 00:46:43 +0100

    Fix: actually disable sourcemaps in vite config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     },
   },
   build: {
-    sourcemap: true,
+    sourcemap: false,
     chunkSizeWarningLimit: 1000,
     target: 'esnext',
     minify: 'esbuild',
